12705b147747e39eb6a6a370faa745e118bf3302
이서연 <iseoyeon@iseoyeons-MacBook-Pro.local>
2019-07-17 22:02:45 +0900

    remove test

--- src/main/java/aeee/api/gasprice/web/controller/GasPriceController.java
+++ src/main/java/aeee/api/gasprice/web/controller/GasPriceController.java
@@ -4,9 +4,7 @@ import aeee.api.gasprice.web.service.GasPriceService;
 import aeee.api.gasprice.web.vo.dto.BlockInfoDTO;
 import aeee.api.gasprice.web.vo.dto.ResponseDTO;
 import aeee.api.gasprice.web.vo.dto.ResponseType;
-import aeee.api.gasprice.web.vo.dto.TestDto;
 import aeee.api.gasprice.web.vo.entity.GasPriceVO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +17,6 @@ public class GasPriceController extends FilterController {
 
     public GasPriceController(GasPriceService gasPriceService){
         this.gasPriceService = gasPriceService;
-    }
-
-    @GetMapping("/hello")
-    public ResponseDTO<TestDto> hello(){
-        return ResponseDTO.get(ResponseType.Success, "", new TestDto());
     }
 
     @GetMapping("")
