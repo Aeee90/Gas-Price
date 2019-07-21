1f5afa3c83ddb161830729e1297833bee91d32d8
이서연 <iseoyeon@iseoyeons-MacBook-Pro.local>
2019-07-21 16:38:18 +0900

    chage Anonymous Classes to Lambda

--- src/main/java/aeee/api/gasprice/web/vo/dto/comparator/TransactionCountDTOComparator.java
+++ src/main/java/aeee/api/gasprice/web/vo/dto/comparator/TransactionCountDTOComparator.java
@@ -5,19 +5,6 @@ import aeee.api.gasprice.web.vo.dto.TransactionCountDTO;
 import java.util.Comparator;
 
 public class TransactionCountDTOComparator {
-
-    public static Comparator<TransactionCountDTO> ComparatorAsc = new Comparator<TransactionCountDTO>() {
-        @Override
-        public int compare(TransactionCountDTO o1, TransactionCountDTO o2) {
-            return o1.getGasPrice().compareTo(o2.getGasPrice());
-        }
-    };
-
-    public static Comparator<TransactionCountDTO> ComparatorDesc = new Comparator<TransactionCountDTO>() {
-        @Override
-        public int compare(TransactionCountDTO o1, TransactionCountDTO o2) {
-            return o2.getGasPrice().compareTo(o1.getGasPrice());
-        }
-    };
-
+    public static Comparator<TransactionCountDTO> ComparatorAsc = (o1, o2) -> o1.getGasPrice().compareTo(o2.getGasPrice());
+    public static Comparator<TransactionCountDTO> ComparatorDesc = (o1, o2) -> o2.getGasPrice().compareTo(o1.getGasPrice());
 }
